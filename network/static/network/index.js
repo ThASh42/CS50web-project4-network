c30ce46532a4dff3a146ff497c8211bfe3a8fa74
ThASh42 <thashmail42@gmail.com>
2024-04-16 18:31:27 +0300

    Create frontend for edit post feature

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -1,3 +1,5 @@
+var editMode = false;
+
 document.addEventListener("DOMContentLoaded", () => {
     const newPostForm = document.getElementById("add-new-post-form");
     const newPostFormCreateButton = newPostForm.querySelector("#add-new-post-form-create-button");
@@ -24,4 +26,36 @@ document.addEventListener("DOMContentLoaded", () => {
             newPostFormCreateButton.disabled = false;
         };
     });
-})
+});
+
+// Edit post button functionality
+function editPost(postId) {
+    if (editMode === false){
+        editMode = true;
+
+        const editTextarea = document.createElement("textarea");
+        const editTextareaDiv = document.createElement("div");
+        const editPostDiv = document.querySelector(`div[data-postId="${postId}"]`);
+        const postContent = editPostDiv.querySelector("#post-content");
+
+        // Div of close and edit buttons
+        const editButton = document.createElement("button");
+        const closeButton = document.createElement("button");
+        const buttonsDiv = document.createElement("div");
+        editButton.classList = ["btn btn-primary mx-1"]; editButton.innerHTML = "Edit";
+        closeButton.classList = ["btn btn-dark mx-1"]; closeButton.innerHTML = "Close";
+        buttonsDiv.appendChild(editButton); buttonsDiv.appendChild(closeButton);
+        buttonsDiv.classList = ["d-flex justify-content-center my-2"]
+        
+        // Textarea
+        editTextarea.innerHTML = postContent.innerHTML;
+        editTextarea.classList = ["form-control"];
+        
+        // Append Childs to the main div
+        editTextareaDiv.appendChild(editTextarea);
+        editTextareaDiv.appendChild(buttonsDiv);
+        editTextareaDiv.classList = ["my-3"];
+
+        editPostDiv.appendChild(editTextareaDiv);
+    };
+};
