ae65a2c3af7e354bdb6fd75c49f1172ccbb804ee
ThASh42 <thashmail42@gmail.com>
2024-04-18 13:52:32 +0300

    Return HttpResponseBadRequest if request is invalid

--- network/views.py
+++ network/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.core.paginator import Paginator
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
@@ -118,7 +118,7 @@ def is_following(request, username):
             followed_user = followed_user).delete()
             return HttpResponse(status=204)
         else:
-            return JsonResponse({"error": "GET, POST or DELETE requests required."}, status=400)
+            return HttpResponseBadRequest("GET, POST or DELETE requests required")
 
 
 def login_view(request):
