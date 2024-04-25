4f6beb08683c435ac64f9049e87ca2086b8a03f6
ThASh42 <thashmail42@gmail.com>
2024-04-25 11:32:52 +0300

    Use icons instead of text in like button

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -1,4 +1,12 @@
 var editMode = false;
+const likeIcon = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+</svg> ` 
+const unlikeIcon = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+</svg> `
 
 document.addEventListener("DOMContentLoaded", () => {
     const newPostForm = document.getElementById("add-new-post-form");
@@ -30,7 +38,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".like-button").forEach(button => {
         const postId = button.dataset.postid;
         const isLiked = like_post_data[postId];
-        if (isLiked) button.innerHTML = "Unlike";
+        if (isLiked) button.innerHTML = unlikeIcon;
         button.onclick = () => likeUnlike(isLiked, postId, button);
     });
 
@@ -124,7 +132,7 @@ function likeUnlike(isLiked, postId, button) {
         usersText.textContent = likeCount.textContent.charAt(likeCountLength - 1) == 1 ? "user" : "users";
 
         // Change button text
-        button.innerHTML = isLiked ? "Like" : "Unlike";
+        button.innerHTML = isLiked ? likeIcon : unlikeIcon;
         
         // Update the button onclick event to toggle like/unlike functionality
         button.onclick = () => likeUnlike(!isLiked, postId, button);
